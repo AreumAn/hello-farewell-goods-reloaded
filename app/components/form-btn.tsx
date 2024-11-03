6fa27c2ee497fd6a9adc8131d8b312c77365db8b
Areum An <areum.an.0928@gmail.com>
2024-11-03 18:06:34 -0500

    Form compoenents

---
+++ app/components/form-btn.tsx
@@ -0,0 +1,15 @@
+interface FormatButtonProps {
+  loading: boolean;
+  text: string;
+}
+
+export default function FormBtn({ loading, text }: FormatButtonProps) {
+  return (
+    <button
+      disabled={loading}
+      className='primary-btn h-10 disabled:bg-neutral-400 disabled:text-neutral-300 disabled:cursor-not-allowed'
+    >
+      {loading ? 'Loading..' : text}
+    </button>
+  );
+}
