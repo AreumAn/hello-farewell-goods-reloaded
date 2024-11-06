03fbc5d1a66b6efcb206b4c5de2ea5a100fcd708
Areum An <areum.an.0928@gmail.com>
2024-11-05 20:22:41 -0500

    zod validation create account

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,27 +8,20 @@ import { useFormState } from 'react-dom';
 import { handleForm } from './action';
 
 export default function Login() {
-  const [state, action] = useFormState(handleForm, null);
+  const [state, dispatch] = useFormState(handleForm, null);
   return (
     <div className='flex flex-col gap-10 py-8 px-6'>
       <div className='flex flex-col gap-2 *:font-medium'>
         <h1 className='text-2xl'>Hello! 👋</h1>
         <h2 className='text-xl'>Log in with email and password</h2>
       </div>
-      <form action={action} className='flex flex-col gap-3'>
-        <FormInput
-          name='email'
-          type='email'
-          placeholder='Email'
-          required
-          errors={[]}
-        />
+      <form action={dispatch} className='flex flex-col gap-3'>
+        <FormInput name='email' type='email' placeholder='Email' required />
         <FormInput
           name='password'
           type='password'
           placeholder='Password'
           required
-          errors={state?.errors ?? []}
         />
         <FormBtn text='Create Account' />
       </form>
