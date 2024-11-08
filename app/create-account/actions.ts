010e1436ebf438a187c150fb8e1ff8528e9de734
Areum An <areum.an.0928@gmail.com>
2024-11-07 22:08:11 -0500

    Transformation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,35 +1,45 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use server';
-
 import { z } from 'zod';
 
-const checkUsername = (username: string) => !username.includes('sibal');
-
-const checkPasswords = ({
-  password,
-  confirm_password,
-}: {
-  password: string;
-  confirm_password: string;
-}) => password === confirm_password;
+const passwordRegex = new RegExp(
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*?[#?!@$%^&*-]).+$/
+);
 
 const formSchema = z
   .object({
     username: z
       .string({
-        invalid_type_error: 'Username must be string!',
-        required_error: 'Where is my username?!',
+        invalid_type_error: 'Username must be a string!',
+        required_error: 'Where is my username???',
       })
-      .min(3, 'way to short!')
-      .max(18, 'toooo long!')
-      .refine(checkUsername, 'No sibal allowed!'),
-    email: z.string().email(),
-    password: z.string().min(10),
-    confirm_password: z.string().min(10),
+      .min(3, 'Way too short!!!')
+      //.max(10, "That is too looooong!")
+      .trim()
+      .toLowerCase()
+      .transform((username) => `🔥 ${username}`)
+      .refine(
+        (username) => !username.includes('potato'),
+        'No potatoes allowed!'
+      ),
+    email: z.string().email().toLowerCase(),
+    password: z
+      .string()
+      .min(4)
+      .regex(
+        passwordRegex,
+        'Passwords must contain at least one UPPERCASE, lowercase, number and special characters #?!@$%^&*-'
+      ),
+    confirm_password: z.string().min(4),
   })
-  .refine(checkPasswords, {
-    message: 'Both passwords should be matched',
-    path: ['confirm_password'],
+  .superRefine(({ password, confirm_password }, ctx) => {
+    if (password !== confirm_password) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'Two passwords should be equal',
+        path: ['confirm_password'],
+      });
+    }
   });
 
 export async function createAccount(prevState: any, formData: FormData) {
@@ -39,9 +49,10 @@ export async function createAccount(prevState: any, formData: FormData) {
     password: formData.get('password'),
     confirm_password: formData.get('confirm_password'),
   };
-
   const result = formSchema.safeParse(data);
   if (!result.success) {
     return result.error.flatten();
+  } else {
+    console.log(result.data);
   }
 }
